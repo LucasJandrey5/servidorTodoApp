4638530c617cfffa4b5df7811e768aaee17980af
lucasjandrey <lucasjandrey5@gmail.com>
2024-01-01 22:07:54 -0300

    feat: createUser criado

--- user/createUser.php
+++ user/createUser.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 $servername = "200.98.129.120:3306";
 $username = 'marcosvir_lucasJ';
 $password = 'H6sJ{Ez7+M0a';
-$database = 'marcosvir_lucasj';
+$database = 'marcosvir_lucasj_todoApp';
 
 // Add the following lines to set CORS headers
 header("Access-Control-Allow-Origin: *"); // Allow requests from any origin (you can restrict this in a production environment)
@@ -16,53 +16,69 @@ header("Access-Control-Allow-Methods: GET"); // Allow POST requests
 header("Access-Control-Allow-Headers: Content-Type"); // Allow Content-Type header
 
 try {
-    $conn = new PDO("mysql:host=$servername; dbname=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn = new PDO("mysql:host=$servername; dbname=$database", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die();
+  echo "Connection failed: " . $e->getMessage();
+  die();
 }
 
 
 $inputFile = file_get_contents('php://input');
 if ($data = json_decode($inputFile, true)) {
 
-    if (json_last_error() === JSON_ERROR_NONE) {
+  if (json_last_error() === JSON_ERROR_NONE) {
 
-        $stmt = $conn->prepare("INSERT INTO restaurante (nome, descricao, endereco, horario) 
-        VALUES (:nome, :descricao, :endereco, :horario)");
 
-        $stmt->bindParam(':nome', $data['nome']);
-        $stmt->bindParam(':descricao', $data['descricao']);
-        $stmt->bindParam(':endereco', $data['endereco']);
-        $stmt->bindParam(':horario', $data['horario']);
-        
-       
-        if ($stmt->execute()) {
-            $response = [
-                "success" => true,
-                "message" => "Data saved successfully."
-            ];
-        } else {
-            $response = [
-                "success" => false,
-                "message" => "Failed to save data."
-            ];
-        }
-    } else {
+    $stmt = $conn->prepare('SELECT email FROM User Where email = :email');
+
+    $stmt->bindParam(':email', $data['email']);
+
+    $stmt->execute();
+
+    $res = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+    if ($res == []) {
+      $stmt = $conn->prepare("INSERT INTO User (username, email, password, profileImage) 
+        VALUES (:username, :email, :password, :profileImage)");
+
+      $stmt->bindParam(':username', $data['username']);
+      $stmt->bindParam(':email', $data['email']);
+      $stmt->bindParam(':password', $data['password']);
+      $stmt->bindParam(':profileImage', $data['profileImage']);
+
+
+      if ($stmt->execute()) {
         $response = [
-            "success" => false,
-            "message" => "Invalid JSON format."
+          "success" => true,
+          "message" => "Data saved successfully.",
+
         ];
-    }
-    
-} else {
-    // No data provided
-    $response = [
+      } else {
+        $response = [
+          "success" => false,
+          "message" => "Failed to save data."
+        ];
+      }
+    } else {
+      $response = [
         "success" => false,
-        "message" => "Input JSON file not found."
+        "message" => "Email already exist!",
+        "data" => $res
+      ];
+    }
+  } else {
+    $response = [
+      "success" => false,
+      "message" => "Invalid JSON format."
     ];
-    
+  }
+} else {
+  // No data provided
+  $response = [
+    "success" => false,
+    "message" => "Input JSON file not found."
+  ];
 }
 
 // Return the JSON response
@@ -71,5 +87,3 @@ echo json_encode($response, JSON_PRETTY_PRINT);
 
 // Close the database connection
 $conn = null;
-
-?>
